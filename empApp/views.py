ec920cb52fe5bb6e7ab8abaa790761aff7140f33
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-14 21:04:32 +0530

    adding views and templates for empApp

--- empApp/views.py
+++ empApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from empApp.models import Employee
+
 
 # Create your views here.
+def employeeData(request):
+    employees = Employee.objects.all()
+    emp_dict = {"employees": employees}
+    return render(request, "empApp/employees.html", emp_dict)
--- modelDemo/urls.py
+++ modelDemo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from empApp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employee-list/', views.employeeData),
 ]
