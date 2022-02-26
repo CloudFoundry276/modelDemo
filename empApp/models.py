6adb1a3def3ff64695c34b829788a872fd0d57fe
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-26 14:29:12 +0530

    implemented Many To One ORM

--- empApp/models.py
+++ empApp/models.py
@@ -9,11 +9,25 @@ class Employee(models.Model):
     email = models.CharField(max_length=50)
 
 
+# Implemented Many To Many ORM
 class Programmer(models.Model):
     name = models.CharField(max_length=30)
     sal = models.FloatField()
 
 
+# Implemented Many To Many ORM
 class Project(models.Model):
     name = models.CharField(max_length=50)
     programmers = models.ManyToManyField(Programmer)
+
+
+# Implemented Many To One ORM
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+
+
+# Implemented Many To One ORM
+class PhoneNumber(models.Model):
+    type = models.CharField(max_length=10)
+    number = models.CharField(max_length=10)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
